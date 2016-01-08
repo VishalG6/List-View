f6af9325085f5accb30ed8efcca3f1482342ce2f
VishalG6 <vishalg_94@hotmail.com>
2016-01-08 22:19:08 +1100

    LIst done.

--- app/src/main/java/com/example/vishal/listview/MainActivity.java
+++ app/src/main/java/com/example/vishal/listview/MainActivity.java
@@ -1,7 +1,14 @@
 package com.example.vishal.listview;
 
+import android.app.ListActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String[] foods = {"Apple", "Banana", "Pear", "Mango", "Watermelon", "Cherry"};
+        ListAdapter vishAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, foods);
+        ListView vishListView = (ListView) findViewById(R.id.vishListView);
+        vishListView.setAdapter(vishAdapter);
+
+        vishListView.setOnItemClickListener(
+                new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        String food = String.valueOf(parent.getItemAtPosition(position));
+                        Toast.makeText(MainActivity.this, food, Toast.LENGTH_LONG).show();
+
+                    }
+                }
+        );
+
     }
 }
